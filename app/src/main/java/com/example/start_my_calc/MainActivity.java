a334d1e4e3637c5b246763a9347f8f75741d2895
Roman Chernega <roman-chernega@yandex.ru>
2021-11-05 15:43:14 +0300

    lesson5_startMyCalc

---
+++ app/src/main/java/com/example/start_my_calc/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.start_my_calc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.calcStart).setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.CATEGORY_APP_CALCULATOR);
+
+            startActivity(intent);
+        });
+    }
+}
